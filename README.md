7a60270e4d19d72ca0756b7410fcf2c15fe95fcf
teffcode <tefy.1@live.com>
2017-05-01 22:21:16 -0500

    add a style for button

---
+++ README.md
@@ -0,0 +1,1 @@
+# BotonConGraficosPersonalizados_
---
+++ app/src/main/java/com/android/teffcode/botoncongraficos/MainActivity.java
@@ -0,0 +1,21 @@
+package com.android.teffcode.botoncongraficos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void sePulsa(View view){
+        Toast.makeText(this,"Pulsado",Toast.LENGTH_SHORT).show();
+    }
+
+}
